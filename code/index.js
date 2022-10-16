f7c97ca2dd35ffb2ce0873052083dce2f2059d0e
Kim Chung <kchung68@my.bcit.ca>
2022-10-15 20:43:03 -0700

    added players movement

--- code/index.js
+++ code/index.js
@@ -1,19 +1,20 @@
-const canvas = document.querySelector('canvas'); 
+const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 1024 
+canvas.width = 1024
 canvas.height = 576 //set canvas size
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.2
+const gravity = 0.7
 
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({ position, velocity }) {
         this.position = position
         this.velocity = velocity
         this.height = 150
         this.width = 50
+        this.lastKey
     }
 
     draw() {
@@ -23,30 +24,36 @@ class Sprite {
 
     update() {
         this.draw()
-        
 
-        if(this.position.y + this.height + this.velocity.y >= canvas.height) { // +this.velocity.y because the stop point will be in the air so that we can use gravity
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) { // +this.velocity.y because the stop point will be in the air so that we can use gravity
             this.velocity.y = 0
         } else {
             this.velocity.y += gravity
         }
 
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
     }
 }
 
 const player = new Sprite({
-    position: {x: 0, y: 0},
-    velocity: {x: 0, y: 0}
+    position: { x: 0, y: 0 },
+    velocity: { x: 0, y: 0 }
 })
 
 const enemy = new Sprite({
-    position: {x: 400, y: 100},
-    velocity: {x: 0, y: 0}
+    position: { x: 400, y: 100 },
+    velocity: { x: 0, y: 0 }
 })
 
-
-console.log(player)
+const keys = {
+    a: { pressed: false },
+    d: { pressed: false },
+    w: { pressed: false },
+    ArrowRight: { pressed: false },
+    ArrowLeft: { pressed: false },
+    ArrowUp: { pressed: false }
+}
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -54,6 +61,77 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    player.velocity.x = 0
+    enemy.velocity.x = 0
+
+    //left player movement
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = -5
+    } else if (keys.d.pressed && player.lastKey === 'd') {
+        player.velocity.x = 5
+    }
+
+    //right player movement
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -5
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = 5
+    }
 }
 
 animate()
+
+window.addEventListener('keydown', (e) => {         //function fired when a key on the keyboard is pressed
+    switch (e.key) {
+        case 'd':
+            keys.d.pressed = true
+            player.lastKey = 'd'
+            break;
+        case 'a':
+            keys.a.pressed = true
+            player.lastKey = 'a'
+            break;
+        case 'w':
+            player.velocity.y = -20
+            break;
+
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
+            break;
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey = 'ArrowLeft'
+            break;
+        case 'ArrowUp':
+            enemy.velocity.y = -20
+            break;
+
+    }
+})
+
+window.addEventListener('keyup', (e) => {         //function fired when we let go of the key
+    switch (e.key) {
+        case 'd':
+            keys.d.pressed = false
+            break;
+        case 'a':
+            keys.a.pressed = false
+            break;
+        case 'ArrowUp':
+            keys.w.pressed = false
+            break;
+
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            break;
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            break;
+        case 'ArrowUp':
+            keys.ArrowUp.pressed = false
+            break;
+
+    }
+})
